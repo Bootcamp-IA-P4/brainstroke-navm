f9d2bdbb95a342f72109d90cc2d42d43814dbe59
Nho89 <noa1720@gmail.com>
2025-06-09 10:37:11 +0200

    feat: added endpoints and database connection

---
+++ server/database/save_user.py
@@ -0,0 +1,11 @@
+from server.database.db_connection import supabase
+
+def save_user(data_dict):
+    try:
+        print("Intentando guardar:", data_dict)
+        response = supabase.table("users").insert(data_dict).execute()
+        print("Paciente guardado correctamente:", response.data)
+        return response.data
+    except Exception as e:
+        print(f"❌ Error al guardar el paciente: {e}")
+        return None
--- server/main.py
+++ server/main.py
@@ -1,6 +1,40 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from server.database.save_user import save_user
+from server.database.db_connection import supabase
 
 app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the FastAPI server!"}
+
+
+class User(BaseModel):
+    gender: bool
+    age: float
+    hypertension: bool
+    heart_disease: bool
+    work_type: str
+    Residence_type: bool
+    avg_glucose_level: float
+    bmi: float
+    smoking_status: str
+    resultado: str
+
+@app.post("/users/")
+def create_user(user: User):
+    data_dict = user.model_dump()
+    result = save_user(data_dict)
+    return {"message": "User saved successfully", "data": result}
+
+@app.get("/users/")
+def get_users():
+    try:
+        response = supabase.table("users").select("*").limit(20).execute()
+        return {"users": response.data}
+    except Exception as e:
+        return {"error": str(e)}
+    
+
+
+# uvicorn server.main:app --reload
