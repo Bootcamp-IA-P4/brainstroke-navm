b211594287c864eb53962bf1db48a803e6048ba5
Nho89 <noa1720@gmail.com>
2025-06-09 14:14:22 +0200

    feat: added testing model

---
+++ tests/test_model.py
@@ -0,0 +1,39 @@
+import unittest
+import pandas as pd
+import joblib
+import os
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+model_path = os.path.join('models', 'model.pkl')
+data_path = os.path.join('data', 'dataset_encoded.csv')
+
+class TestModel(unittest.TestCase):
+    def setUp(self):
+        self.model = joblib.load(model_path)
+        
+    def test_model_loaded(self):
+        self.assertIsNotNone(self.model, "Model should be loaded successfully.")
+
+    def test_predictino_shape(self):
+        df = pd.read_csv(data_path)
+        X = df.drop(columns=['stroke'])
+        y_pred = self.model.predict(X)
+        self.assertEqual(len(y_pred), len(X), "Prediction should match the number of input samples.")
+
+    def test_prediction_values(self):
+        df = pd.read_csv(data_path)
+        X = df.drop(columns=['stroke'])
+        y_pred = self.model.predict(X)
+        self.assertTrue(set(y_pred).issubset({0, 1}), "La predicción debería tener 0 o 1")
+
+    def test_minimum_accuracy(self):
+        df = pd.read_csv(data_path)
+        X = df.drop(columns=['stroke'])
+        y = df['stroke']
+        y_pred = self.model.predict(X)
+        accuracy = accuracy_score(y, y_pred)
+        self.assertGreaterEqual(accuracy, 0.7, "La precisión del modelo debería ser al menos 70%.")
+
+if __name__ == '__main__':
+    unittest.main()
